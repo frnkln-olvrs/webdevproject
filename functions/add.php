34f2653c64c4fda5a1676c660b8513cff24ce56a
frnkln-olvrs <franklinoliveros2003@gmail.com>
2023-10-30 19:07:12 +0800

    Change Name
    Fixing error for adding data

--- functions/add.php
+++ functions/add.php
@@ -9,22 +9,22 @@
   <?php 
     include_once("../dbConnection/mysqlconfig_connection.php");
 
-    if(isset($_POST['Submit'])) {
+    if(isset($_POST['submit'])) {
       $code = $_POST['code'];
       $name = $_POST['name'];
 
       if(empty($code) || empty($name)) {
         if(empty($code)) {
-          echo "<font color = 'red'> ubject code field is empty.<font><br>";
+          echo "<font color = 'red'> object code field is empty.</font><br>";
         }
         if(empty($name)) {
-          echo "<font color = 'red'> ubject code field is empty.<font><br>";
+          echo "<font color = 'red'> object code field is empty.</font><br>";
         }
 
         echo "<br><a href='javascript:self.history.back();'>Go Back</a>";
       }
       else {
-        $result = mysqli_query($dbc, "INSERT INTO tblsubject(subject_code, subject_name) VALUES('$code', '$name')");
+        $result = mysqli_query($dbc, "INSERT INTO tblsubjects(Subject_Code, Subject_Name) VALUES('$code', '$name')");
           
         echo "<font color='green'>Data added succesfully.";
         echo "<br><a href='../index.php'>View Result</a>";
