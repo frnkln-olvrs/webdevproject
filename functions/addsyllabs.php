efab82df8e5f9fa10467aef97017b0d3b5f4f926
frnkln-olvrs <franklinoliveros2003@gmail.com>
2023-11-22 15:21:27 +0800

    Add syllabus

---
+++ functions/addsyllabs.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <title>Add Syllabus</title>
+  </head>
+  <body>
+    <?php 
+      include_once("../dbConnection/mysqlconfig_connection.php");
+
+      if(isset($_POST['Submit'])) {
+        $code = $_POST['$code'];
+        $author = $_POST['$author'];
+        $subject = $_POST['$subject'];
+
+        if (empty($code) || empty($author)) {
+          if(empty($code)) {
+            echo "<font color='red'>Syllabus Code field is empty</font<<br/>";
+          }
+          if(empty($author)) {
+            echo "<font color='red'>Syllabus Author field is empty</font<<br/>";
+          }
+          echo "<br/><a href='javascript:self.hhistory.back();'>Go back</a>";
+        }
+        else {
+          $result = mysqli_query($dbc, $query);
+          echo "<font color='green'>Data added Succesfully</font>";
+          echo "<br/><a href='../index.php;'>View Result</a>";
+        }
+      }
+    ?>
+  </body>
+</html>
