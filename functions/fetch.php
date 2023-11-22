93fed82de698c3c1f50398930f6fe3dd87b0f837
frnkln-olvrs <franklinoliveros2003@gmail.com>
2023-11-22 14:53:46 +0800

    update select query
    update the SELECT query from fetch.php

--- functions/fetch.php
+++ functions/fetch.php
@@ -1,6 +1,6 @@
 <?php 
   //fetching data
-  $query = "SELECT * FROM tblsubjects";
+  $query = "SELECT * FROM tblsubjects LEFT JOIN tblsyllabus ON tblsyllabus.subject_id = tblsubjects.subjectid;";
   //using mysqli_query
   $result = mysqli_query($dbc, $query); //using mysql_query
 ?>
