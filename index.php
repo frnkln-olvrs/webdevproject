1e5d6b4c263925e4db2a386875a43a3958f0914e
frnkln-olvrs <franklinoliveros2003@gmail.com>
2023-11-22 13:56:25 +0800

    Syllabus/ fetching data

--- index.php
+++ index.php
@@ -19,6 +19,9 @@
       <td>ID</td>
       <td>Subject Code</td>
       <td>Subject Name</td>
+      <td>Syllabus ID</td>
+      <td>Syllabus Code</td>
+      <td>Syllabus Author</td>
       <td>Action</td>
     </tr>
 
@@ -30,6 +33,9 @@
         echo "<td>".$res['Subject_ID']."</td>";
         echo "<td>".$res['Subject_Code']."</td>";
         echo "<td>".$res['Subject_Name']."</td>";
+        echo "<td>".$res['syllabus_id']."</td>";
+        echo "<td>".$res['syllabus_code']."</td>";
+        echo "<td>".$res['syllabus_athor']."</td>";
         echo "<td><a href=\"forms/editform.php?id=$res[Subject_ID]\">Edit</a> |
                   <a href=\"functions/delete.php?id=$res[Subject_ID]\"
                   onclick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
